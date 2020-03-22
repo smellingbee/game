a51c46bc90f9367843e4ca896692e17094d4371a
Jacob Stein <jacobstein123@gmail.com>
2020-03-22 13:31:19 -0500

    Updates

---
+++ precache-manifest.737eaf2f27bd3b2522512e33e346037a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab18d514c6bee5fedfceadd45a3b2a08",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "8395f1d82ad598912986",
+    "url": "/game/static/css/main.7b303d3d.chunk.css"
+  },
+  {
+    "revision": "26e6c2420f7ab3c82712",
+    "url": "/game/static/js/2.2fbe453e.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/game/static/js/2.2fbe453e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8395f1d82ad598912986",
+    "url": "/game/static/js/main.b70f57f4.chunk.js"
+  },
+  {
+    "revision": "a40722839c4d24b99514",
+    "url": "/game/static/js/runtime-main.c5dba578.js"
+  }
+]);
